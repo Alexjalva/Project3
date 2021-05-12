8fc050a736c87db6441bdb3f521c87b475893ca7
Carlos Castillo <castillo25ca@gmail.com>
2021-05-12 12:40:09 -0700

    fix

--- project3/models/user.js
+++ project3/models/user.js
@@ -20,10 +20,6 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    github: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
