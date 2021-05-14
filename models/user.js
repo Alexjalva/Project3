78879617e5108942164dc9739ff4f271cd59931f
Carlos Castillo <castillo25ca@gmail.com>
2021-05-13 21:38:05 -0700

    changes to user.js

--- models/user.js
+++ models/user.js
@@ -1,62 +1,52 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
-
-User.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    username: {
+// Creating our User model
+module.exports = function(sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+    // Name(string) -- display name
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // Email(string) -- unique identifier for logging in
     email: {
       type: DataTypes.STRING,
-      allowNull: false,
       unique: true,
+      allowNull: false,
       validate: {
         isEmail: true,
       },
     },
+    // Password(string) -- hashed password for logging in
     password: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        len: [4],
-      },
     },
-  },
-  {
-    hooks: {
-      async beforeCreate(newUserData) {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      async beforeUpdate(updatedUserData) {
-        updatedUserData.password = await bcrypt.hash(
-          updatedUserData.password,
-          10
-        );
-        return updatedUserData;
-      },
-    },
-
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "user",
-  }
-);
-
-module.exports = User;
+    // color to use in calendar and such
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "#007bff",
+    }
+  });
+  User.associate = function (models) {
+    User.belongsTo(models.Household, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+    User.hasMany(models.Chore);
+    User.hasMany(models.Repetition, {
+      onDelete: "cascade"
+    });
+  };
+  // a method to verify the password
+  User.prototype.verifyPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // hash the password before saving it
+  User.addHook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+  });
+  return User;
+};
