a8a8156ba6ee09ae7c43860e20a228e6d5d157f0
Carlos Castillo <castillo25ca@gmail.com>
2021-05-15 11:54:01 -0700

    update odds model folder

--- models/odds.js
+++ models/odds.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const oddsSchema = new Schema({
+const OddsSchema = new Schema({
   name: { type: String, required: true },
   selected: {type: Boolean, default: false },
   // username : [{type: Schema.Types.ObjectId, ref: 'Users' }] //trying to get user view to work
