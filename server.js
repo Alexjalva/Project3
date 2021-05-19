94fd338da07f0816feac38e4797e8f83cd2e9bec
Carlos Castillo <castillo25ca@gmail.com>
2021-05-18 18:46:47 -0700

    fix

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const passport = require("passport");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const routes = require("./routes");
