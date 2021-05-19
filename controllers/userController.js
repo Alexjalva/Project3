5d90d8e38fa9042012d4cdcc45bc68d2157d221f
Carlos Castillo <castillo25ca@gmail.com>
2021-05-18 20:52:18 -0700

    updates to userController file

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,37 +1,60 @@
 const db = require("../models");
 
-// Defining methods for the booksController
+// Defining methods for the userController
 module.exports = {
-  findAll: function(req, res) {
-    db.Book
-      .find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+  getUser: (req, res, next) => {
+    console.log('===== user!!======');
+    console.log(req.user);
+    if (req.user) {
+      return res.json({ user: req.user });
+    } else {
+      return res.json({ user: null });
+    }
   },
-  findById: function(req, res) {
-    db.Book
-      .findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+  register: (req, res) => {
+    const { firstName, lastName, username, password } = req.body;
+    // ADD VALIDATION
+    db.User.findOne({ 'username': username }, (err, userMatch) => {
+      if (userMatch) {
+        return res.json({
+          error: `Sorry, already a user with the username: ${username}`
+        });
+      }
+      const newUser = new db.User({
+        'firstName': firstName,
+        'lastName': lastName,
+        'username': username,
+        'password': password
+      });
+      newUser.save((err, savedUser) => {
+        if (err) return res.json(err);
+        return res.json(savedUser);
+      });
+    });
   },
-  create: function(req, res) {
-    db.Book
-      .create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+  logout: (req, res) => {
+    if (req.user) {
+      console.log("Logout");
+      req.session.destroy();
+      res.clearCookie('connect.sid'); // clean up!
+      return res.json({ msg: 'logging you out' });
+    } else {
+      return res.json({ msg: 'no user to log out!' });
+    }
   },
-  update: function(req, res) {
-    db.Book
-      .findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+  auth: function(req, res, next) {
+		console.log(req.body);
+		console.log('================');
+		next();
   },
-  remove: function(req, res) {
-    db.Book
-      .findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }
+  authenticate: (req, res) => {
+		console.log('POST to /login');
+		const user = JSON.parse(JSON.stringify(req.user)); // hack
+		const cleanUser = Object.assign({}, user);
+		if (cleanUser) {
+			console.log(`Deleting ${cleanUser.password}`);
+			delete cleanUser.password;
+		}
+		res.json({ user: cleanUser });
+	}
 };
