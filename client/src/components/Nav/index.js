a3504c503bf60fae0feece3199fc1ad2e5cd0aef
Carlos Castillo <castillo25ca@gmail.com>
2021-05-25 20:20:54 -0700

    test

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-function Nav() {
+const Nav = (props) => {
+  let greeting;
+
+  if (props.user === null) {
+		greeting = <p>Hello guest</p>
+	} else if (props.user.firstName) {
+		greeting = (
+			<Fragment>
+				Welcome, <strong>{props.user.firstName}</strong>
+			</Fragment>
+		)
+	} else if (props.user.username) {
+		greeting = (
+			<Fragment>
+				Welcome, <strong>{props.user.username} </strong>
+			</Fragment>
+		)
+  }
+  
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        <strong>Bet on It</strong>
-      </a>
-    </nav>
-  );
-}
+    <nav className="navbar navbar-expand-lg">
+      <div className="app-name">BET ON IT</div>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon">   
+        <i className="fa fa-navicon"></i>
+      </span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/favoriterecipes">EVENTS</a>
+          </li>
+        </ul>
+        <div className="float-right">
+          {greeting}&emsp;&emsp;  
+          <a href="/" className="logout" onClick={props.logout}>Logout</a>
+        </div>
+      </div>
+    </nav>    
+  )
+};
 
 export default Nav;
