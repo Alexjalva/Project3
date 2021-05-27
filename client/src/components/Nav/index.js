3e6a068506e3ca29ae1b9f32148294aef0c2513a
Carlos Castillo <castillo25ca@gmail.com>
2021-05-26 17:59:07 -0700

    style change to nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,28 +1,17 @@
 import React from "react";
+import "./style.css"
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="app-name">BET ON IT</div>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon">   
-        <i className="fa fa-navicon"></i>
-      </span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="/">LOGIN <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/Events">MY STUFF</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/SignUp">   REGISTER</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div class="topnav" id="myTopnav">
+  <a href="/Events">BET ON IT!</a>
+  <a href="/">LOGIN</a>
+  <a href="/EventDetail">MY BETS</a>
+  <a href="/SignUp">REGISTER</a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
   );
 }
 
