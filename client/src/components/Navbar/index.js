268ce769f0aaa49621dddf507af3116e517c2148
Carlos Castillo <castillo25ca@gmail.com>
2021-05-26 21:21:52 -0700

    fix logout

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./style.css"
+import AUTH from "../../utils/AUTH";
 
 function Navbar() {
   return (
     <div class="topnav" id="myTopnav">
   <a href="/">BET ON IT!</a>
   <a href="/Events">MY STUFF!</a>
-  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-    <i class="fa fa-bars"></i>
-  </a>
+  <a href="/Login" onClick={AUTH.logout}>Log out!</a>
 </div>
   );
 }
