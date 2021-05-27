e387d3ab172f0ae20ce990923527fde18f729de3
Carlos Castillo <castillo25ca@gmail.com>
2021-05-26 20:02:52 -0700

    style change

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -4,7 +4,7 @@ import "./style.css"
 function Jumbotron() {
   return (
     <div class="jumbotron">
-  <h1>TRYING TO WIN BIG ON SPORTS!</h1>
+  <h1>TRYING TO WIN BIG ON SPORTS?!</h1>
   <p>SIGN UP TODAY AND GET FREE BETTING ODDS!</p>
 </div>
   );
